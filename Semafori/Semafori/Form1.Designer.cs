f9bb1310a3e2ca49e5b4491ee4ef582cd9fecf13
Gaidar-Sensei <34005660+Gaidar-eromanga-Sensei@users.noreply.github.com>
2020-06-21 11:03:54 +0500

    Add files via upload

---
+++ Semafori/Semafori/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Semafori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(160, 139);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Здесь будет выводиться сгенерированный массив";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(78, 198);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(211, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Использование критических секций";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(190, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(160, 139);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Здесь будет выводиться результат сортировки массива";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(190, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Отсортировать массив";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(338, 37);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сброс";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 270);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Потоковая сортировка массива";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Semafori/Semafori/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semafori
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int[] array = new int[100];
+        public static int[] SortedArray = new int[100]; 
+
+        Thread BubbleThread = new Thread (bubbleSort);
+        Thread MaxSortThread = new Thread (MaxSort);
+        Thread BubbleThreadWithoutLock = new Thread (bubbleSortWithoutLock);
+        Thread MaxSortThreadWithoutLock = new Thread (MaxSortWithoutLock);
+
+        static object locker = new object();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Random ran = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = ran.Next(1, 100);
+                textBox1.Text += array[i] + " ";
+            }
+            button1.Enabled = false;
+            button2.Enabled = true;
+            checkBox1.Enabled = true;
+        }
+        public static void bubbleSort()
+        {
+            SortedArray = array;
+            lock (locker)
+            {
+                for (int i = 0; i < SortedArray.Length; i++)
+                {
+                    for (int j = i + 1; j < SortedArray.Length; j++)
+                    {
+                        if (SortedArray[i] > SortedArray[j])
+                        {
+                            int t = SortedArray[i];
+                            SortedArray[i] = SortedArray[j];
+                            SortedArray[j] = t;
+                        }
+                    }
+                }
+            }
+
+        }
+        public static void bubbleSortWithoutLock()
+        {
+            SortedArray = array;
+            for (int i = 0; i < SortedArray.Length; i++)
+            {
+                for (int j = i + 1; j < SortedArray.Length; j++)
+                {
+                    if (SortedArray[i] > SortedArray[j])
+                    {
+                        int t = SortedArray[i];
+                        SortedArray[i] = SortedArray[j];
+                        SortedArray[j] = t;
+                    }
+                }
+            }
+        }
+        public static void MaxSort()
+        {
+            lock (locker)
+            {
+                SortedArray = array;
+                for (int i = 0; i < SortedArray.Length - 1; i++)
+                {
+                    int min = i;
+                    for (int j = i + 1; j < SortedArray.Length; j++)
+                    {
+                        if (SortedArray[j] < SortedArray[min])
+                        {
+                            min = j;
+                        }
+                    }
+                    int temp = SortedArray[min];
+                    SortedArray[min] = SortedArray[i];
+                    SortedArray[i] = temp;
+                }
+            }
+        }
+        public static void MaxSortWithoutLock()
+        {
+            SortedArray = array;
+            for (int i = 0; i < SortedArray.Length - 1; i++)
+            {
+                int min = i;
+                Thread.Sleep(1000);
+                for (int j = i + 1; j < SortedArray.Length; j++)
+                {
+                    if (SortedArray[j] < SortedArray[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = SortedArray[min];
+                SortedArray[min] = SortedArray[i];
+                SortedArray[i] = temp;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            if (checkBox1.Checked == true)
+            {
+                BubbleThreadWithoutLock.Start();
+                MaxSortThreadWithoutLock.Start();
+            }
+            else
+            {
+                BubbleThread.Start();
+                MaxSortThread.Start();
+            }
+            for (int i = 0; i < SortedArray.Length; i++)
+                textBox2.Text += SortedArray[i] + " ";
+            button2.Enabled = false;
+            checkBox1.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button1.Enabled = true;
+            textBox1.Text = "Здесь будет выводиться сгенерированный массив";
+            textBox2.Text = "Здесь будет выводиться результат сортировки массива";
+        }
+    }
+}
